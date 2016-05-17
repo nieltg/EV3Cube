c5181cfc58f92576b9ab245bd17c155c5cef59fb
Daniel <nieltg@users.noreply.github.com>
2016-05-18 04:28:24 +0700

    app: Modify appearance and icons

--- app/src/main/java/nieltg/ev3cube/MainActivity.java
+++ app/src/main/java/nieltg/ev3cube/MainActivity.java
@@ -20,8 +20,10 @@ public class MainActivity extends AppCompatActivity
 	{
 		super.onCreate (savedInstanceState);
 		setContentView (R.layout.activity_main);
+
 		Toolbar toolbar = (Toolbar) findViewById (R.id.toolbar);
 		setSupportActionBar (toolbar);
+		getSupportActionBar ().setDisplayShowTitleEnabled (false);
 
 		FloatingActionButton fab = (FloatingActionButton) findViewById (R.id.fab);
 		fab.setOnClickListener (new View.OnClickListener ()
